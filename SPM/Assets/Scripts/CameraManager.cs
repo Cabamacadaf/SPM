9d7a8264e1f271a4632a10d35288fd3c06d7ba0c
Simon <simon.sundstrom95@gmail.com>
2019-04-09 14:30:37 +0200

    Camera

--- SPM/Assets/Scripts/CameraManager.cs
+++ SPM/Assets/Scripts/CameraManager.cs
@@ -11,42 +11,27 @@ public class CameraManager : MonoBehaviour
     [SerializeField] private float maxClampValue;
 
     [SerializeField] private float zoomSensitivity;
-    private float mouseSensitivity;
-    [SerializeField] private float sensitivity;
+    [SerializeField] private float mouseSensitivity;
 
 
     private Vector3 movement;
 
-    private Vector3 cameraOffset = new Vector3(0, 2.0f, -7.0f);
+    [SerializeField] private Vector3 cameraOffset;
     private SphereCollider sphereCollider;
     public LayerMask geometryLayer;
 
 
-    enum State
-    {
-        STATE_THIRDPERSON,
-        STATE_FIRSTPERSON,
-        STATE_ZOOMED
-
-    }
-
-    private State state;
 
     // Start is called before the first frame update
     private void Awake()
     {
         sphereCollider = GetComponent<SphereCollider>();
-        state = State.STATE_THIRDPERSON;
 
     }
-    void Start()
-    {
-    }
-
+  
     // Update is called once per frame
     void Update()
     {
-        HandleUpdate();
 
         rotationX -= Input.GetAxisRaw("Mouse Y") * mouseSensitivity;
 
@@ -62,50 +47,7 @@ public class CameraManager : MonoBehaviour
         transform.position = movement + transform.parent.position;
     }
 
-    private void HandleUpdate()
-    {
-        switch (state)
-        {
-            case State.STATE_THIRDPERSON:
-                if (Input.GetKeyDown(KeyCode.Mouse1))
-                {
-                    state = State.STATE_FIRSTPERSON;
-                }
-                minClampValue = -45;
-                maxClampValue = 45;
-                cameraOffset = new Vector3(0, 2.0f, -7.0f);
-                mouseSensitivity = sensitivity;
-                break;
-            case State.STATE_FIRSTPERSON:
-                if (Input.GetKeyDown(KeyCode.Mouse1))
-                {
-                    state = State.STATE_ZOOMED;
-                }
-                else if (Input.GetKeyDown(KeyCode.Escape))
-                {
-                    state = State.STATE_THIRDPERSON;
-                }
-                minClampValue = -90;
-                maxClampValue = 90;
-                mouseSensitivity = sensitivity;
-                cameraOffset = new Vector3(0, 0, 0);
-                break;
-            case State.STATE_ZOOMED:
-                if (Input.GetKeyDown(KeyCode.Mouse1))
-                {
-                    state = State.STATE_FIRSTPERSON;
-                }
-                else if (Input.GetKeyDown(KeyCode.Escape))
-                {
-                    state = State.STATE_THIRDPERSON;
-                }
-                minClampValue = -90;
-                maxClampValue = 90;
-                cameraOffset = new Vector3(0, 0, 7);
-                mouseSensitivity = zoomSensitivity;
-                break;
-        }
-    }
+
 
     private void CheckCollision()
     {
