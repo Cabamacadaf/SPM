0a7e44876dff4c6ce69307ccdd8d4f6fe07d9adc
Marcus M <maccus9@hotmail.com>
2019-04-09 15:45:36 +0200

    updated prefab, added gravity gun stuff

---
+++ SPM/Assets/PickUpObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpObject : MonoBehaviour
+{
+    private bool active = false;
+    private Transform player;
+    private float pullForce;
+
+    void Awake()
+    {
+        player = FindObjectOfType<PlayerController3D>().transform;
+    }
+    
+    void Update()
+    {
+        if(active == true) {
+            transform.position += (player.position - transform.position).normalized * pullForce * Time.deltaTime;
+        }
+    }
+
+    public void Pull (float pullForce)
+    {
+        this.pullForce = pullForce;
+        active = true;
+    }
+}
--- SPM/Assets/Scripts/GravityGun.cs
+++ SPM/Assets/Scripts/GravityGun.cs
@@ -4,19 +4,44 @@ using UnityEngine;
 
 public class GravityGun : MonoBehaviour
 {
-    [SerializeField] private float range;
-    [SerializeField] private float force;
+    [SerializeField] private float pushRange;
+    [SerializeField] private float pullRange;
+    [SerializeField] private float pushForce;
+    [SerializeField] private float pullForce;
     [SerializeField] private LayerMask hitLayer;
-   public void Shoot ()
+    [SerializeField] private Transform firePoint;
+    [SerializeField] private Transform pullPoint;
+    
+    public void Push ()
     {
-        Debug.Log("Shoot");
-        RaycastHit hit;
-        if(Physics.Raycast(transform.position, Vector3.forward, out hit, range, hitLayer)) {
+        Debug.Log("Push");
+        if(Physics.Raycast(firePoint.position, firePoint.forward, out RaycastHit hit, pushRange, hitLayer)) {
             Debug.Log("Hit");
+            Debug.DrawLine(firePoint.position, hit.point, Color.red, 2);
             if (hit.collider.attachedRigidbody != null) {
-                hit.collider.attachedRigidbody.AddForce(hit.normal * force);
+                Debug.Log(1-(hit.distance / pushRange));
+                hit.collider.attachedRigidbody.AddForce(firePoint.forward * pushForce * (1-(hit.distance / pushRange)));
             }
         }
-        Debug.DrawRay(transform.position, hit.point, Color.red, 2);
+        else {
+            Debug.DrawRay(firePoint.position, firePoint.forward * pushRange, Color.blue, 2);
+        }
+    }
+
+    public void Pull ()
+    {
+        Debug.Log("Pull");
+        if (Physics.Raycast(firePoint.position, firePoint.forward, out RaycastHit hit, pullRange, hitLayer)) {
+            Debug.Log("Hit");
+            Debug.DrawLine(firePoint.position, hit.point, Color.green, 2);
+
+            hit.collider.GetComponent<PickUpObject>().Pull(pullForce);
+            //if (hit.collider.attachedRigidbody != null) {
+            //    hit.collider.attachedRigidbody.AddForce(-firePoint.forward * pullForce * (1-(hit.distance / pullRange)));
+            //}
+        }
+        else {
+            Debug.DrawRay(firePoint.position, firePoint.forward * pushRange, Color.blue, 2);
+        }
     }
 }
