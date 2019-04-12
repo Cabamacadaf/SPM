b5b4a040ac54d016dc6e7145a8efa913004d8769
Simon <simon.sundstrom95@gmail.com>
2019-04-12 14:35:30 +0200

    Explosion

---
+++ SPM/Assets/Scripts/Enemy/States/Enemy2AggroState.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy2AggroState : EnemyAggroState
+{
+    [SerializeField] private float chaseDistance = 6.0f;
+    public override void Enter()
+    {
+        base.Enter();
+    }
+
+    public override void HandleUpdate()
+    {
+        base.HandleUpdate();
+        owner.transform.position += new Vector3(Mathf.PingPong(Time.time, 3), owner.transform.position.y, owner.transform.position.z);
+        
+        //MoveTowardPlayer();
+    }
+}
