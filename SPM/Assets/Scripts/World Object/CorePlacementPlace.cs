13cf847cfe1f90a27f91246b0cbab0900f8b12b0
Marcus M <maccus9@hotmail.com>
2019-04-25 10:22:55 +0200

    början på powercore

---
+++ SPM/Assets/Scripts/World Object/CorePlacementPlace.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorePlacementPlace : MonoBehaviour
+{
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("PowerCore")) {
+            other.transform.parent = transform;
+        }
+    }
+}
