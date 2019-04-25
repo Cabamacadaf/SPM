2b38c5e58a1639585e3aebdff2d215ed729d27f0
Ghostwolf7 <37661407+Ghostwolf7@users.noreply.github.com>
2019-04-26 00:35:18 +0200

    Vi byter ljus nu!
    Men det är inte perfekt, ljuset får fel värde och lyser förstarkt
    Men det är i all fall nån feedback på vad vi gör!

--- SPM/Assets/Scripts/World Object/CorePlacementPlace.cs
+++ SPM/Assets/Scripts/World Object/CorePlacementPlace.cs
@@ -7,6 +7,12 @@ public class CorePlacementPlace : MonoBehaviour
     public GameObject objectA;
     private GameObject objectB;
 
+    //Color color0 = Color.blue;
+    Color color1 = new Color(0f, 145f, 0f, 5f);
+
+    public Light lt;
+
+
     private void OnTriggerEnter (Collider other)
     {
         if (other.CompareTag("PowerCore"))
@@ -17,6 +23,9 @@ public class CorePlacementPlace : MonoBehaviour
             objectB.transform.parent = objectA.transform;
             GameController.gameControllerInstance.powerCoreCollection++;
             GameObject.Find("Objective_Power_Core").GetComponent<PickUpObject>().enabled = false;
+            //stäng av Rigided body
+            //byt ljusen till grönt!
+            lt.color = (color1 / 2.0f) * Time.deltaTime;
         }
     }
 }
