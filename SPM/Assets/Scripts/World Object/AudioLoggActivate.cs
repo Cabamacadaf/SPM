e870812d38ab896c49c3d7e0b283702e97cc6e3a
Ghostwolf7 <37661407+Ghostwolf7@users.noreply.github.com>
2019-04-26 15:31:18 +0200

    Audio logg prefab
    AudioLogg prefab added
    PlacerHolder Sound added
    Scriptet kommer behöva förbättras inför riktiga spelet
    Ändrat på massan på vassa objekt

---
+++ SPM/Assets/Scripts/World Object/AudioLoggActivate.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioLoggActivate : MonoBehaviour
+{
+    private AudioSource source;
+
+    public AudioClip loggClip;
+    
+    void Start()
+    {
+        source = GetComponent<AudioSource>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.CompareTag("Player") && !source.isPlaying) // ska ha keypress down
+        {
+            source.PlayOneShot(loggClip, 1.0f);
+        }
+    }
+}
