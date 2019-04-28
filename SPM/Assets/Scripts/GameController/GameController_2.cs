5d23618f4142254eb2efa5ab0201f2ba7ec69616
Ghostwolf7 <37661407+Ghostwolf7@users.noreply.github.com>
2019-04-29 00:44:15 +0200

    Dags att testa på lvl 2 withebox
    Rip me >.<
    lvl 2.2 fungerar inte som den ska, måste alså testa på orginalet direkt ....

---
+++ SPM/Assets/Scripts/GameController/GameController_2.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.UI;
+
+public class GameController_2 : MonoBehaviour
+{
+    public static GameController_2 gameControllerInstance_2;
+    //public GameObject keyCard;
+    //public Transform keySpawnPoint;
+    [HideInInspector]
+    public bool hasKeycard;
+    public GameObject door_Goal;
+
+    [HideInInspector] //ni får skriva det här med eran andra version
+    public int powerCoreCollection;
+
+    void Start()
+    {
+        gameControllerInstance_2 = this;
+        powerCoreCollection = 0;
+        hasKeycard = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (powerCoreCollection == 16)
+        {
+            //spawna keyCard
+            OpenDoor();
+            powerCoreCollection = 0;
+
+        }
+    }
+
+    void OpenDoor()
+    {
+        door_Goal.SetActive(true);
+        return;
+    }
+}
---
+++ SPM/Assets/Scripts/World Object/ControlPanel2.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPanel2 : MonoBehaviour
+{
+    public GameObject door;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (door == isActiveAndEnabled)
+            {
+                door.SetActive(false);
+                return;
+            }
+
+            else if (door.activeInHierarchy == false)
+            {
+                door.SetActive(true);
+                return;
+            }
+
+        }
+    }
+}
