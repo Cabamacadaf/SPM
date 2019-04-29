53398b1e26b716ec5cd29c437b67eb1cffd43291
Kerstin521 <37629767+Kerstin521@users.noreply.github.com>
2019-04-29 08:44:24 +0200

    Lvl.2 WhiteBox Knapp
    Stora rummet, Knappen och ljuskällor

--- SPM/Assets/Scripts/World Object/ControlPanel2.cs
+++ SPM/Assets/Scripts/World Object/ControlPanel2.cs
@@ -1,23 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControlPanel2 : MonoBehaviour
 {
     public GameObject door;
+    private GameObject button;
+    public Light light1, light2, light3, light4, light5;
+    public Text eButton;
+    //public GameObject doorLight;
+
+    private void Start()
+    {
+        light1.enabled = false;
+        light2.enabled = false;
+        light3.enabled = false;
+        light4.enabled = false;
+        light5.enabled = false;
+        eButton.enabled = false;
+        button = this.gameObject;
+        //doorLight.SetActive(false);
+    }
 
     private void OnTriggerStay(Collider other)
     {
-
+        eButton.enabled = true;
         if (Input.GetKeyDown(KeyCode.E))
         {
             if (GameController_2.gameControllerInstance_2.hasKeycard)
             {
                 door.SetActive(false);
+                light1.enabled = true;
+                light2.enabled = true;
+                light3.enabled = true;
+                light4.enabled = true;
+                light5.enabled = true;
                 Debug.Log("I now Open the door for you");
+                eButton.enabled = false;
+                Vector3 tmp = button.transform.position;
+                tmp.z = (tmp.z - 0.5f) ;
+                button.transform.position = tmp;
+                //doorLight.SetActive(true);
             }
         }
-
         //if (Input.GetKeyDown(KeyCode.E))
         //{
         //    if (door == isActiveAndEnabled)
@@ -34,4 +60,6 @@ public class ControlPanel2 : MonoBehaviour
 
         //}
     }
+
+   
 }
