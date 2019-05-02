1cc73fddcd6bfb7e723eb48211dc4054fe43a209
Simon <simon.sundstrom95@gmail.com>
2019-05-02 15:03:17 +0200

    Gravity gun

--- SPM/Assets/Scripts/Player/GravityGun/GravityGun.cs
+++ SPM/Assets/Scripts/Player/GravityGun/GravityGun.cs
@@ -5,81 +5,35 @@ using UnityEngine.UI;
 
 public class GravityGun : StateMachine
 {
-    [SerializeField] private float pushRange;
-    [SerializeField] private float pullRange;
+    public float pushRange;
+    public float pullRange;
     public float pushForce;
-    [SerializeField] private float pullForce;
-    [SerializeField] private float cameraOffset;
+    public float pullForce;
+    public float cameraOffset;
 
-    [SerializeField] private LayerMask hitLayer;
+    public LayerMask hitLayer;
 
-    [SerializeField] private Image crosshair;
+    public Image crosshair;
 
-    [HideInInspector] public PickUpObject holdingObject;
+    [HideInInspector]
+    public PickUpObject holdingObject;
 
-    [SerializeField] private float playerPushForce;
+    public float playerPushForce;
 
     //Power up
     [SerializeField] private float powerUpLength;
     [SerializeField] private float powerUpMultiplier;
 
 
-    public void Update()
+
+    protected override void Awake()
     {
-        if (Physics.Raycast(Camera.main.transform.position + Camera.main.transform.forward * cameraOffset, Camera.main.transform.forward, out RaycastHit hit, pushRange, hitLayer) && hit.transform.GetComponent<PickUpObject>() != null)
-        {
-            crosshair.color = Color.green;
-        }
-        else if (Physics.Raycast(Camera.main.transform.position + Camera.main.transform.forward * cameraOffset, Camera.main.transform.forward, out RaycastHit hitInfo, pullRange, hitLayer) && hit.transform.GetComponent<PickUpObject>() != null)
-        {
-            crosshair.color = Color.green;
-        }
-        else
-        {
-            crosshair.color = Color.red;
-        }
+
+
+        base.Awake();
+
     }
 
-    public void Push()
-    {
-        if (holdingObject == null)
-        {
-            if (Physics.Raycast(Camera.main.transform.position + Camera.main.transform.forward * cameraOffset, Camera.main.transform.forward, out RaycastHit hit, pushRange, hitLayer) && hit.transform.GetComponent<PickUpObject>() != null)
-            {
-                if (hit.collider.attachedRigidbody != null && hit.collider.GetComponent<PickUpObject>() != null)
-                {
-                    hit.collider.attachedRigidbody.AddForce(Camera.main.transform.forward * pushForce * (1 - (hit.distance / pushRange)));
-                }
-            }
-
-        }
-
-        else if (holdingObject.holding)
-        {
-            holdingObject.Drop();
-            holdingObject.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * pushForce);
-            holdingObject = null;
-        }
-    }
-
-    public void Pull()
-    {
-        if (holdingObject == null)
-        {
-            if (Physics.Raycast(Camera.main.transform.position + Camera.main.transform.forward * cameraOffset, Camera.main.transform.forward, out RaycastHit hit, pullRange, hitLayer) && hit.transform.GetComponent<PickUpObject>() != null)
-            {
-                holdingObject = hit.collider.GetComponent<PickUpObject>();
-                holdingObject.Pull(pullForce);
-            }
-
-        }
-
-        else
-        {
-            holdingObject.Drop();
-            holdingObject = null;
-        }
-    }
 
     private IEnumerator PowerDownRoutine()
     {
