c72b922cf3148bcd852ad97ca09011e4022036b2
Simon <simon.sundstrom95@gmail.com>
2019-05-03 16:35:09 +0200

    Player

--- SPM/Assets/Scripts/Player/States/PlayerRunState.cs
+++ SPM/Assets/Scripts/Player/States/PlayerRunState.cs
@@ -20,7 +20,19 @@ public class PlayerRunState : PlayerGroundState
 
         if(owner.Movement.GetVelocity().magnitude < owner.Movement.RunningSpeed)
         {
-            owner.Movement.AddVelocity(direction * owner.Movement.Acceleration * Time.deltaTime);
+            float dotProduct = Vector3.Dot(owner.Movement.GetVelocity().normalized, direction);
+            Debug.Log("DotProduct: " + dotProduct);
+            if (dotProduct <= 0)
+            {
+                Debug.Log("Turnspeed");
+                owner.Movement.AddVelocity(direction * owner.Movement.Acceleration * turnSpeedModifier * Time.deltaTime);
+            }
+            else
+            {
+                owner.Movement.AddVelocity(direction * owner.Movement.Acceleration * Time.deltaTime);
+
+            }
+
 
         }
 
