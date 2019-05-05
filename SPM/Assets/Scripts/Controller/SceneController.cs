587274ec55886821c75b542c506b31aae5a3e1db
Simon <simon.sundstrom95@gmail.com>
2019-05-05 16:12:35 +0200

    Restart

---
+++ SPM/Assets/Scripts/Controller/SceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneController : MonoBehaviour
+{
+    public KeyCode RestartSceneButton = KeyCode.P;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(RestartSceneButton))
+        {
+            RestartScene();
+        }
+    }
+
+    private void RestartScene()
+    {
+        Scene currentScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
+    }
+}
