fa7550b779d96e7af30ce97f0624619bce08391a
Cabamacadaf <maccus9@hotmail.com>
2019-05-07 16:37:58 +0200

    audiolog

---
+++ SPM/Assets/Scripts/World Object/AudioLog.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioLog : InteractiveObject
+{
+    private AudioSource audioSource;
+
+    [SerializeField] private AudioClip audioLogClip;
+
+    private bool played = false;
+    
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        base.Awake();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && interactive && !played) {
+            played = true;
+            audioSource.PlayOneShot(audioLogClip);
+        }
+    }
+}
