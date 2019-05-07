5c7c8ef3cc8722b7ac435f32d69903e479141ea4
Ghostwolf7 <37661407+Ghostwolf7@users.noreply.github.com>
2019-05-08 01:00:21 +0200

    Simon - Jag har Commenterat ut IDDrawerUIE för den compilerar inte!
    Och då gick det inte ens att test köra spelet :(
    B_Labb klart, med triggers, extra ljuskällor och checkpoint
    C och D kvar att göra
    Plus ventilation...

--- SPM/Assets/Scripts/Help Classes/IDDrawerUIE.cs
+++ SPM/Assets/Scripts/Help Classes/IDDrawerUIE.cs
@@ -3,22 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.IMGUI;
-using UnityEngine.UI
+using UnityEngine.UI;
 
 
 
-[CustomPropertyDrawer(typeof(Ingredient))]
-public class IDDrawerUIE : proper
-{
-    // Start is called before the first frame update
-    void Start()
-    {
+//[CustomPropertyDrawer(typeof(Ingredient))]
+//public class IDDrawerUIE : proper
+//{
+//    // Start is called before the first frame update
+//    void Start()
+//    {
         
-    }
+//    }
 
-    // Update is called once per frame
-    void Update()
-    {
+//    // Update is called once per frame
+//    void Update()
+//    {
         
-    }
-}
+//    }
+//}
