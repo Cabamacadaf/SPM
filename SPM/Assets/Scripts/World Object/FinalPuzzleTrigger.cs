bbffcbda9379c67453d51e07a0bec34d4c29c2dd
Simon <simon.sundstrom95@gmail.com>
2019-05-07 23:07:26 +0200

    Sista pusslet

--- SPM/Assets/Scripts/World Object/FinalPuzzleTrigger.cs
+++ SPM/Assets/Scripts/World Object/FinalPuzzleTrigger.cs
@@ -1,34 +1,46 @@
-﻿using System.Collections;
+﻿//Author: Simon Sundström
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class FinalPuzzleTrigger : MonoBehaviour
 {
-   
+    [SerializeField] private List<Light> lights;
+
+    
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("PuzzleObject"))
+     
+        if (other.CompareTag("PuzzleObject") && other.GetComponent<ID>().NR == GetComponent<ID>().NR)
         {
-            
-            other.transform.position = transform.position;
-            other.transform.parent = transform;
-            other.transform.rotation = Quaternion.identity;
-            other.gameObject.layer = 0;
-            Destroy(other.GetComponent<Rigidbody>());
-            
-            GameController_2.gameControllerInstance_2.greenCollecting++;
-            //StartCoroutine(ChangeColors());
 
-            //objectB = other.transform.gameObject;
-            //objectB.transform.position = objectA.transform.position;
-            //objectB.transform.parent = objectA.transform;
-            //GameController.gameControllerInstance.powerCoreCollection++;
-            //GameObject.Find("Objective_Power_Core").GetComponent<PickUpObject>().enabled = false;
-            ////stäng av Rigided body
-            //objectB.gameObject.SetActive(false);
-            ////byt ljusen till grönt!
-            //lt.color = (color1 / 2.0f) * Time.deltaTime;
+            TransformObject(other);
+
+            //Bestäm vad ljusen ska göra!!
+            HandleLight();
+
+            Debug.Log("?????");
+            GameController_2.gameControllerInstance_2.greenCollecting++;
+
+        }
+    }
+
+    private void TransformObject(Collider other)
+    {
+        other.transform.position = transform.position;
+        other.transform.parent = transform;
+        other.transform.rotation = Quaternion.identity;
+    
+    }
+
+    private void HandleLight()
+    {
+        foreach(Light light in lights)
+        {
+            //Gör det du vill göra med lights här
         }
     }
 }
