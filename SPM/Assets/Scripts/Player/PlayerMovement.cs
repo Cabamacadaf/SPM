092a181fdc3d8895d5a0eeef228769cc7db0e8d2
Simon <simon.sundstrom95@gmail.com>
2019-05-08 14:40:43 +0200

    Testa nu
    Testa och sprinta nu med Shift

--- SPM/Assets/Scripts/Player/PlayerMovement.cs
+++ SPM/Assets/Scripts/Player/PlayerMovement.cs
@@ -32,11 +32,11 @@ public class PlayerMovement : PhysicsComponent
     // Update is called once per frame
     void Update()
     {
-        if(groundAngle > 95)
-        {
-            dynamicFrictionCoefficient = 0.5f;
+        //if(groundAngle > 95)
+        //{
+        //    dynamicFrictionCoefficient = 0.5f;
 
-        }
+        //}
 
         ApplyGravity();
         CheckCollision();
