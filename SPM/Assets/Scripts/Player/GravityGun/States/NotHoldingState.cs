58fc87d7a39d11b0fdb7711b5db3e355ba8bcfac
Cabamacadaf <maccus9@hotmail.com>
2019-05-08 22:04:05 +0200

    formatering

--- SPM/Assets/Scripts/Player/GravityGun/States/NotHoldingState.cs
+++ SPM/Assets/Scripts/Player/GravityGun/States/NotHoldingState.cs
@@ -10,25 +10,23 @@ public class NotHoldingState : State
     private PickUpObject lastPickUpObjectHit;
     private GravityBlast gravityBlast;
 
-    public override void Initialize(StateMachine owner)
+    public override void Initialize (StateMachine owner)
     {
         this.owner = (GravityGun)owner;
         gravityBlast = owner.GetComponent<GravityBlast>();
     }
 
-    public override void Enter()
+    public override void Enter ()
     {
         base.Enter();
     }
 
-    public override void HandleUpdate()
+    public override void HandleUpdate ()
     {
         Physics.Raycast(Camera.main.transform.position + Camera.main.transform.forward * owner.cameraOffset, Camera.main.transform.forward, out RaycastHit hit, owner.pushRange, owner.hitLayer);
 
-        if (hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer("Pick Up Objects"))
-        {
-            if (lastPickUpObjectHit != null && hit.transform.gameObject != lastPickUpObjectHit)
-            {
+        if (hit.collider != null && hit.collider.gameObject.layer == LayerMask.NameToLayer("Pick Up Objects")) {
+            if (lastPickUpObjectHit != null && hit.transform.gameObject != lastPickUpObjectHit) {
                 lastPickUpObjectHit.UnHighlight();
             }
             lastPickUpObjectHit = hit.transform.GetComponent<PickUpObject>();
@@ -36,52 +34,44 @@ public class NotHoldingState : State
             owner.crosshair.color = Color.green;
         }
 
-        else
-        {
-            if (lastPickUpObjectHit != null)
-            {
+        else {
+            if (lastPickUpObjectHit != null) {
                 lastPickUpObjectHit.UnHighlight();
             }
             owner.crosshair.color = Color.red;
         }
 
-        if (hit.collider != null && hit.collider.CompareTag("Enemy"))
-        {
+        if (hit.collider != null && hit.collider.CompareTag("Enemy")) {
             owner.crosshair.color = Color.yellow;
         }
 
-        if (Input.GetMouseButtonDown(0))
-        {
+        if (Input.GetMouseButtonDown(0)) {
             Push();
         }
 
-        if (Input.GetMouseButtonDown(1))
-        {
+        if (Input.GetMouseButtonDown(1)) {
             Pull();
         }
     }
 
-    public void Push()
+    public void Push ()
     {
-        if (Physics.Raycast(Camera.main.transform.position + Camera.main.transform.forward * owner.cameraOffset, Camera.main.transform.forward, out RaycastHit hit, owner.pushRange, owner.hitLayer) && hit.collider.attachedRigidbody != null)
-        {
-            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Pick Up Objects"))
-            {
+        if (Physics.Raycast(Camera.main.transform.position + Camera.main.transform.forward * owner.cameraOffset, Camera.main.transform.forward, out RaycastHit hit, owner.pushRange, owner.hitLayer)
+            && hit.collider.attachedRigidbody != null) {
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Pick Up Objects")) {
                 // hit.collider.attachedRigidbody.isKinematic = false;
                 hit.collider.attachedRigidbody.AddForce(Camera.main.transform.forward * owner.pushForce * (1 - (hit.distance / owner.pushRange)));
             }
-            else if (hit.collider.gameObject.CompareTag("Platform"))
-            {
+            else if (hit.collider.gameObject.CompareTag("Platform")) {
                 Platform platform = hit.collider.gameObject.GetComponent<Platform>();
-                if (platform.IsActive)
-                {
+                if (platform.IsActive) {
                     hit.collider.gameObject.GetComponent<Platform>().IsActive = false;
                 }
             }
         }
     }
 
-    public void Pull()
+    public void Pull ()
     {
 
         if (Physics.Raycast(Camera.main.transform.position + Camera.main.transform.forward * owner.cameraOffset, Camera.main.transform.forward, out RaycastHit hit, owner.pullRange, owner.hitLayer)) {
