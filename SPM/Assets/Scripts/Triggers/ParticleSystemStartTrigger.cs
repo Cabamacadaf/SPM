952b6568a01d7d31862be9f4e14f3741cc94d4a4
Cabamacadaf <maccus9@hotmail.com>
2019-05-14 14:15:07 +0200

    particle system trigger

---
+++ SPM/Assets/Scripts/Triggers/ParticleSystemStartTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemStartTrigger : MonoBehaviour
+{
+    [SerializeField] private ParticleSystem[] particleSystems;
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            ActivateParticleSystems();
+        }
+    }
+
+    private void ActivateParticleSystems ()
+    {
+        foreach (ParticleSystem particleSystem in particleSystems) {
+            particleSystem.Play();
+        }
+    }
+}
