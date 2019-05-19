4ac7847bcc0182ced2099e1c17c7e987e9afe46e
Cabamacadaf <maccus9@hotmail.com>
2019-05-19 20:30:20 +0200

    prefabs

---
+++ SPM/Assets/Scripts/Objectives/Final Puzzle/FinalPuzzleDoorOpenButton.cs
@@ -0,0 +1,20 @@
+﻿//Author: Marcus Mellström
+
+using UnityEngine;
+
+public class FinalPuzzleDoorOpenButton : InteractiveObject
+{
+    [SerializeField] private Door door;
+
+    private void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && IsInteractive && GameController.Instance.Level2PuzzleComplete) {
+            if (door.IsClosed) {
+                door.Open();
+            }
+            else if (door.IsOpen) {
+                door.Close();
+            }
+        }
+    }
+}
---
+++ SPM/Assets/Scripts/Objectives/Final Puzzle/FinalPuzzleTrigger.cs
@@ -0,0 +1,60 @@
+﻿//Author: Simon Sundström
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class FinalPuzzleTrigger : MonoBehaviour
+{
+    private bool isActive = true;
+
+
+    private GravityGun gravityGun;
+
+    private void Awake()
+    {
+        gravityGun = FindObjectOfType<GravityGun>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+     
+        if (isActive && other.CompareTag("PuzzleObject") && other.GetComponent<ID>().NR == GetComponent<ID>().NR)
+        {
+            Debug.Log("Collided");
+
+            isActive = false;
+            if (gravityGun.HoldingObject != null)
+            {
+                GravityGunBaseState gravityGunState = (GravityGunBaseState)gravityGun.GetCurrentState();
+                gravityGunState.DropObject(false);
+            }
+
+            TransformObject(other);
+
+            GameController.Instance.AddLastPuzzle();
+
+        }
+    }
+
+    //private void OnTriggerExit (Collider other)
+    //{
+    //    if (other.CompareTag("PuzzleObject") && other.GetComponent<ID>().NR == GetComponent<ID>().NR) {
+
+    //        GameController.Instance.AddLastPuzzle();
+
+    //    }
+    //}
+
+    private void TransformObject(Collider other)
+    {
+  
+        Destroy(other.GetComponent<Rigidbody>());
+        other.transform.position = transform.position;
+        other.transform.parent = transform;
+        other.transform.rotation = Quaternion.identity;
+        other.gameObject.layer = 0;
+
+    }
+}
