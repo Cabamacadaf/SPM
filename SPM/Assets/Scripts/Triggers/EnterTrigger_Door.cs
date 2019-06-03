eec556d21e72224d4956524edf6b735ec5f87c42
Kerstin521 <37629767+Kerstin521@users.noreply.github.com>
2019-06-03 12:20:22 +0200

    ändrade lite i lvl2

---
+++ SPM/Assets/Scripts/Triggers/EnterTrigger_Door.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterTrigger_Door : MonoBehaviour
+{
+    [SerializeField] private Door door;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter (Collider other)
+    {
+        if (other.CompareTag("Player")) {
+            door.Open();
+        }
+    }
+}
